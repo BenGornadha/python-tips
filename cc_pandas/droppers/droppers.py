2627a05012f3a8147961878ee6bf626ddfd46bbb
BenGornad <benjamin.gornadha@gmail.com>
2024-10-15 23:17:10 +0200

    ok

--- cc_pandas/droppers/droppers.py
+++ cc_pandas/droppers/droppers.py
@@ -5,13 +5,13 @@ from cc_pandas.droppers.port import Dropper
 
 
 class RowDropper(Dropper):
-    def __init__(self, minimum_missing_percentage_accepted: Optional[float] = None):
-        self.minimum_missing_percentage_accepted = minimum_missing_percentage_accepted
+    def __init__(self, maximum_missing_percentage_accepted: Optional[float] = None):
+        self.max_missing_percentage_accepted = maximum_missing_percentage_accepted
 
     def _compute_minimum_non_missing_values_accepted(self, nb_columns: int) -> int:
-        if self.minimum_missing_percentage_accepted is None:
+        if self.max_missing_percentage_accepted is None:
             return nb_columns
-        return int((1 - self.minimum_missing_percentage_accepted) * nb_columns) + 1 # + 1 is for odd nb cols
+        return int((1 - self.max_missing_percentage_accepted) * nb_columns) + 1 # + 1 is for odd nb cols
 
     def drop(self, df: pd.DataFrame) -> pd.DataFrame:
         self._require_non_empty_dataframe(df=df)
@@ -20,13 +20,13 @@ class RowDropper(Dropper):
 
 
 class ColumnDropper(Dropper):
-    def __init__(self, minimum_missing_percentage_accepted: Optional[float] = None):
-        self.minimum_missing_percentage_accepted = minimum_missing_percentage_accepted
+    def __init__(self, maximum_missing_percentage_accepted: Optional[float] = None):
+        self.max_missing_percentage_accepted = maximum_missing_percentage_accepted
 
     def _compute_minimum_non_missing_values_accepted(self, nb_rows: int) -> int:
-        if self.minimum_missing_percentage_accepted is None:
+        if self.max_missing_percentage_accepted is None:
             return nb_rows
-        return int((1 - self.minimum_missing_percentage_accepted) * nb_rows)
+        return int((1 - self.max_missing_percentage_accepted) * nb_rows)
 
     def drop(self, df: pd.DataFrame) -> pd.DataFrame:
         self._require_non_empty_dataframe(df=df)
--- cc_pandas/droppers/main_classic.py
+++ cc_pandas/droppers/main_classic.py
@@ -10,13 +10,11 @@ if __name__ == '__main__':
         "Phone Number": [None, "555-5678", "555-9876"]
     })
 
+    threshold_row = int(0.5 * len(df.columns))
+    df_cleaned_rows = df.dropna(axis=0, thresh=threshold_row)
+
     threshold_col = int(0.5 * len(df))
     df_cleaned_columns = df.dropna(axis=1, thresh=threshold_col)
 
-    threshold_row = int(0.5 * len(df.columns))
-    df_cleaned_rows = df_cleaned_columns.dropna(axis=0, thresh=threshold_row)
-
-    print("DataFrame après avoir supprimé les colonnes avec plus de 50% de NaN :")
-    print(df_cleaned_columns)
     print("\nDataFrame après avoir supprimé les lignes avec plus de 50% de NaN :")
     print(df_cleaned_rows)
